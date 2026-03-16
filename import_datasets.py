b78691efd0c1b8ee17dc4f985af51fa8a5ab3d1e
krzysztof-m-w <krzysztof.m.weber@gmail.com>
2026-03-16 10:40:31 +0100

    fixes

---
+++ import_datasets.py
@@ -0,0 +1,23 @@
+import json
+
+import numpy as np
+import os
+
+dataset_path = "data/datasets"
+destination_path = "ts2vec/datasets"
+
+dataset_names = []
+
+for filename in os.listdir(dataset_path):
+    if filename.endswith(".npz"):
+        file_path = os.path.join(dataset_path, filename)
+        # Process the dataset file
+        data = np.load(file_path)
+        X = data["X"]
+        dataset_name = filename[:-4]
+        dataset_names.append(dataset_name)
+        save_path = os.path.join(destination_path, dataset_name + ".npy")
+        np.save(save_path, X)
+
+with open("ts2vec/datasets/dataset_names.json", "w") as f:
+    json.dump(dataset_names, f)
